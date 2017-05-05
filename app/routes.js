1867e274b90b05553648ae3592e3179d73ffce26
Nhan Hoang <siege250294@gmail.com>
2017-05-05 16:09:14 +0700

    app: add navigation bar for application

--- app/routes.js
+++ app/routes.js
@@ -1,13 +1,30 @@
 import HomeScreen from './components/screens/HomeScreen'
 import TestScreen from './components/screens/TestScreen'
+import {
+    Navigator
+} from 'react-native'
 
 export default {
     home: {
         title: 'Home',
-        scene: HomeScreen
+        scene: HomeScreen,
+        config: Navigator.SceneConfigs.PushFromRight,
+        rightButton: {
+            title: 'Noti',
+            onPress(navigator) {
+                console.log('press noti button')
+            }
+        }
     },
     test: {
         title: 'Test',
-        scene: TestScreen
+        scene: TestScreen,
+        config: Navigator.SceneConfigs.FloatFromBottom,
+        rightButton: {
+            title: 'Noti',
+            onPress(navigator) {
+                console.log('press noti button')
+            }
+        }
     }
 }
--- app/styles/index.js
+++ app/styles/index.js
@@ -2,5 +2,24 @@ import { StyleSheet } from 'react-native'
 import colors from './constants'
 
 export default StyleSheet.create({
-    
+    navBar: {
+        flex: 1,
+        backgroundColor: 'green',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    navBarTitle: {
+        padding: 8,
+        color: 'white',
+        fontWeight: '600',
+    },
+    navBarTextLeft: {
+        padding: 8,
+        color: 'white',
+    },
+    navBarTextRight: {
+        padding: 8,
+        color: 'white',
+    }
+
 })
